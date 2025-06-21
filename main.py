d6e29761bb61380af2101ef859a710afc12b737a
karo-24lina <u64728@student.wat.edu.pl>
2025-06-21 22:32:20 +0200

    feat: add read functionality

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-tekst - "Geoinformatyka rządzi"
-print(tekst)
+users:list=[
+]
+
+
+def add_user(users_data: list)->None:
+
+         new_user_name=input('Podaj imię nowego znajomego: ')
+         new_location=input('Podaj miasto pochodzenia nowego znajomego: ')
+         new_posts=int(input('Podaj ilość postów nowego znajomego: '))
+         users_data.append({'name':new_user_name,'location':new_location,'posts:':new_posts})
+add_user(users)
+
+print(users)
